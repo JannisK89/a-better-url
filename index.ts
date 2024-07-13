72363d19ce4159c79e76c98135458e4aa711770c
JannisK89 <jannis.karanikis@gmail.com>
2024-07-13 12:08:53 +0200

    base

--- index.ts
+++ index.ts
@@ -1,1 +1,52 @@
-console.log("Hello via Bun!");
+export type BURL = {
+  base: string
+  directories: string[]
+  params: Record<string, string>
+  url(): string
+  overrideParams(params: Record<string, string>): BURL
+  options: Options
+}
+
+type Options = {
+  encodeParams: boolean
+}
+
+const defaultOptions: Options = {
+  encodeParams: false,
+}
+
+function mergeOptions(options?: Options): Options {
+  return { ...defaultOptions, ...options }
+}
+
+export function bURL(
+  base: string,
+  directories: string[],
+  params: Record<string, string>,
+  options?: Options
+): BURL {
+  const finalizedOptions = mergeOptions(options)
+  return {
+    base,
+    directories,
+    params,
+    url: url,
+    overrideParams: overrideParams,
+    options: finalizedOptions,
+  }
+}
+
+function url(this: BURL) {
+  return `${this.base}/${this.directories.join('/')}?${Object.entries(
+    this.params
+  )
+    .map(
+      ([k, v]) =>
+        `${k}=${this.options.encodeParams ? encodeURIComponent(v) : v}`
+    )
+    .join('&')}`
+}
+
+function overrideParams(this: BURL, params: Record<string, string>) {
+  return bURL(this.base, this.directories, { ...this.params, ...params })
+}
